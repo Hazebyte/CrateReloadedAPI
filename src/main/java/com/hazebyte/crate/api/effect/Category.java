4cda22f2b42c2796cb508d8664ddc785add0a3e1
William <imwillx@users.noreply.github.com>
2018-02-10 21:07:06 -0800

    Add docs for category

--- src/main/java/com/hazebyte/crate/api/effect/Category.java
+++ src/main/java/com/hazebyte/crate/api/effect/Category.java
@@ -1,8 +1,23 @@
 package com.hazebyte.crate.api.effect;
 
 public enum Category {
+    /**
+     * Called when a player opens a crate.
+     */
     OPEN(),
+
+    /**
+     * Consistently runs at a block.
+     */
     PERSISTENT(),
+
+    /**
+     * Consistently run when a player is undergoing an animation.
+     */
     ANIMATION(),
+
+    /**
+     * Called when an animation ends.
+     */
     END();
 }
