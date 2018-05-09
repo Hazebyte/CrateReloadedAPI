0db1cbafb5dabb4d3d91b97175fa28d98636ee70
William <imwillx@users.noreply.github.com>
2018-05-08 22:13:54 -0700

    Update Messages

---
+++ src/main/java/com/hazebyte/crate/api/util/Links.java
@@ -0,0 +1,12 @@
+package com.hazebyte.crate.api.util;
+
+/**
+ * Used to guide users to documentation.
+ */
+public interface Links {
+    String CRATE_TYPES  =   "";
+    String ITEMS        =   "";
+    String META         =   "";
+    String ANIMATION    =   "";
+    String TAG          =   "";
+}
--- src/main/java/com/hazebyte/crate/api/util/Messages.java
+++ src/main/java/com/hazebyte/crate/api/util/Messages.java
@@ -5,10 +5,13 @@ package com.hazebyte.crate.api.util;
  */
 public interface Messages {
 
-    String UNAVAILABLE_FEATURE  =   "The following feature is not available: %s";
-    String MESSAGE_NOT_FOUND    =   "The following message key was not found: ";
-
-    String RELOAD               =   "The plugin has been reloaded.";
-    String ERROR                =   "An error has occurred: ";
-    String ERROR_LINE           =   ERROR + "Parsing (%s)";
+    String UNAVAILABLE_FEATURE          =   "The following feature is not available: %s";
+    String UNAVAILABLE_FEATURE_INFO     =   "The following feature is not available: %s [see %s]";
+    String MESSAGE_NOT_FOUND            =   "The following message key was not found: ";
+    String RELOAD                       =   "The plugin has been reloaded.";
+    String ERROR                        =   "An error has occurred: ";
+    String ERROR_LINE                   =   ERROR + "Parsing (%s)";
+    String ERROR_LINE_INFO              =   ERROR + "Parsing (%s) [see %s]";
+    String FORMAT                       =   "Please check the format: ";
+    String FORMAT_ITEM                  =   FORMAT + "ITEM (%s) [see %s]";
 }
