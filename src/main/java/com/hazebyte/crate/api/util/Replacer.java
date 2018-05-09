9388893a1c8bd0058c81b007f64387b91ba192b9
William <imwillx@users.noreply.github.com>
2018-05-08 18:14:16 -0700

    Update Variables

--- src/main/java/com/hazebyte/crate/api/util/Replacer.java
+++ src/main/java/com/hazebyte/crate/api/util/Replacer.java
@@ -10,49 +10,40 @@ import java.util.List;
 public class Replacer {
 
     public static String replace(String string) {
-        string = string.replaceAll("%prefix%", Messenger.getPrefix())
-                .replaceAll("%p%", Messenger.getPrefix())
-                .replaceAll("%list%", CrateAPI.getCrateRegistrar() != null ? CrateAPI.getCrateRegistrar().getCrateString() : "");
+        string = string.replace("{prefix}", Messenger.getPrefix())
+                .replace("{p}", Messenger.getPrefix())
+                .replace("{list}", CrateAPI.getCrateRegistrar() != null ? CrateAPI.getCrateRegistrar().getCrateString() : "");
 
-        string = string.replace("%aqua%", "&b")
-                .replace("%black%", "&0")
-                .replace("%blue%", "&9")
-                .replace("%dark_aqua%", "&3")
-                .replace("%dark_blue%", "&1")
-                .replace("%dark_grey%", "&8")
-                .replace("%dark_green%", "&2")
-                .replace("%dark_purple%", "&5")
-                .replace("%dark_red%", "&4")
-                .replace("%gold%", "&6")
-                .replace("%gray%", "&7")
-                .replace("%green%", "&a")
-                .replace("%light_purple%", "&d")
-                .replace("%red%", "&c")
-                .replace("%white%", "&f")
-                .replace("%yellow%", "&e")
-                .replace("%bold%", "&l")
-                .replace("%italic%", "&o")
-                .replace("%magic%", "&k")
-                .replace("%reset%", "&r")
-                .replace("%strike%", "&m")
-                .replace("%strikethrough%", "&m")
-                .replace("%underline%", "&n");
+        string = string.replace("{aqua}", "&b")
+                .replace("{black}", "&0")
+                .replace("{blue}", "&9")
+                .replace("{dark_aqua}", "&3")
+                .replace("{dark_blue}", "&1")
+                .replace("{dark_grey}", "&8")
+                .replace("{dark_green}", "&2")
+                .replace("{dark_purple}", "&5")
+                .replace("{dark_red}", "&4")
+                .replace("{gold}", "&6")
+                .replace("{gray}", "&7")
+                .replace("{green}", "&a")
+                .replace("{light_purple}", "&d")
+                .replace("{red}", "&c")
+                .replace("{white}", "&f")
+                .replace("{yellow}", "&e")
+                .replace("{bold}", "&l")
+                .replace("{italic}", "&o")
+                .replace("{magic}", "&k")
+                .replace("{reset}", "&r")
+                .replace("{strike}", "&m")
+                .replace("{strikethrough}", "&m")
+                .replace("{underline}", "&n");
         string = replace(string, '&');
         return string;
     }
 
     public static String replace(String string, Player player) {
-        return string.replaceAll("%player%", player.getName())
-                .replaceAll("%playerUUID%", player.getUniqueId().toString());
-    }
-
-    public static String replace(String string, Crate crate) {
-        return string.replaceAll("%crateName%", crate.getDisplayName())
-                .replaceAll("%crateUUID%", crate.getCrateName())
-                .replaceAll("%itemName%", ItemHelper.getName(crate.getItem()))
-                .replaceAll("%crateType%", crate.getType().name())
-                .replaceAll("%crateCost%", Double.toString(crate.getCost()))
-                .replaceAll("%crateRewards%", crate.getRewardString());
+        return string.replace("{player}", player.getName())
+                .replace("{playerUUID}", player.getUniqueId().toString());
     }
 
     public static String replace(String string, char color) {
