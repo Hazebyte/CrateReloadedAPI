0dbfcd4a8342f6d59a24db13250809f651e89077
William <imwillx@users.noreply.github.com>
2018-05-20 00:47:22 -0700

    Add Links

--- src/main/java/com/hazebyte/crate/api/util/Links.java
+++ src/main/java/com/hazebyte/crate/api/util/Links.java
@@ -4,9 +4,9 @@ package com.hazebyte.crate.api.util;
  * Used to guide users to documentation.
  */
 public interface Links {
-    String CRATE_TYPES  =   "";
-    String ITEMS        =   "";
-    String META         =   "";
-    String ANIMATION    =   "";
-    String TAG          =   "";
+    String CRATE_TYPES  =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=crate-types";
+    String ITEMS        =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=item-parser";
+    String META         =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=item-parser";
+    String ANIMATION    =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=animations";
+    String TAG          =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=tag";
 }
