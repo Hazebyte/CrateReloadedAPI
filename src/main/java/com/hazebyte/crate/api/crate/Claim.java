cd7ac4ce103ca4e1f64f7dd5f1ab2e6c6d056fbc
William <imwillx@users.noreply.github.com>
2018-06-22 23:48:47 -0700

    Add Claim Support

---
+++ src/main/java/com/hazebyte/crate/api/crate/Claim.java
@@ -0,0 +1,74 @@
+package com.hazebyte.crate.api.crate;
+
+import com.hazebyte.crate.api.CrateAPI;
+import com.hazebyte.crate.api.crate.reward.Reward;
+
+import java.util.*;
+
+public class Claim {
+    private List<Reward> rewards;
+    private UUID uuid;
+    private long timestamp;
+
+    public Claim(UUID uuid, List<Reward> rewards) {
+        this.uuid = uuid;
+        this.rewards = rewards;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public Claim(UUID uuid, Reward... rewards) {
+        this.uuid = uuid;
+        this.rewards = Arrays.asList(rewards);
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public Claim(UUID uuid, long timestamp, List<Reward> rewards) {
+        this.uuid = uuid;
+        this.rewards = rewards;
+        this.timestamp = timestamp;
+    }
+
+    public Claim(UUID uuid, long timestamp, Reward... rewards) {
+        this.uuid = uuid;
+        this.rewards = Arrays.asList(rewards);
+        this.timestamp = timestamp;
+    }
+
+    public List<Reward> getRewards() {
+        return rewards;
+    }
+
+    public Reward getFirstReward() {
+        if (rewards == null) {
+            return null;
+        }
+        return rewards.get(0);
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public List<String> serialize() {
+        List<String> rewardsAsString = new LinkedList<>();
+        this.rewards.forEach(reward -> {
+            rewardsAsString.add(reward.serialize());
+        });
+        return rewardsAsString;
+    }
+
+    public static Claim parse(UUID uuid, String timestamp, List<String> rewardLines) {
+        List<Reward> rewards = new ArrayList<>();
+        for (String string: rewardLines) {
+            Reward reward = CrateAPI.getCrateRegistrar().createReward(string);
+            rewards.add(reward);
+        }
+
+        Claim claim = new Claim(uuid, Long.valueOf(timestamp), rewards);
+        return claim;
+    }
+}
---
+++ src/main/java/com/hazebyte/crate/api/event/ClaimEvent.java
@@ -0,0 +1,44 @@
+package com.hazebyte.crate.api.event;
+
+import com.hazebyte.crate.api.crate.Claim;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Represents an event that is called when a player tries to claim a reward.
+ */
+public class ClaimEvent extends Event implements Cancellable {
+
+    private Claim claim;
+    private boolean cancelled;
+
+    private static HandlerList handlerList = new HandlerList();
+
+    public ClaimEvent(Claim claim) {
+        this.claim = claim;
+    }
+
+    public Claim getClaim() {
+        return claim;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+}
