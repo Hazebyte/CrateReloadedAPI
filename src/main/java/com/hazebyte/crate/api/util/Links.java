d6a37eab11d247d6fc5fab0f6c7c5dbf77ab7348
William <imwillx@users.noreply.github.com>
2018-09-03 21:35:52 -0700

    Update Links

--- src/main/java/com/hazebyte/crate/api/util/Links.java
+++ src/main/java/com/hazebyte/crate/api/util/Links.java
@@ -4,9 +4,10 @@ package com.hazebyte.crate.api.util;
  * Used to guide users to documentation.
  */
 public interface Links {
-    String CRATE_TYPES  =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=crate-types";
-    String ITEMS        =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=item-parser";
-    String META         =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=item-parser";
-    String ANIMATION    =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=animations";
-    String TAG          =   "https://hazebyte.github.io/CrateReloaded/#/config/crate?id=tag";
+    String CRATE_TYPES  =   "https://crates.hazebyte.com/#/config/crate?id=crate-types";
+    String ITEMS        =   "https://crates.hazebyte.com/#/config/crate?id=item-parser";
+    String META         =   "https://crates.hazebyte.com/#/config/crate?id=item-parser";
+    String ANIMATION    =   "https://crates.hazebyte.com/#/config/crate?id=animations";
+    String TAG          =   "https://crates.hazebyte.com/#/config/crate?id=tag";
+    String MATERIAL     =   "https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Material.html";
 }
