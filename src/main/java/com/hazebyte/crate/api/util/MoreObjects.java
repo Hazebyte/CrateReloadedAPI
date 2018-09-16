e09599cdc06c06b12f2ef6298f323ae2aac249a9
William <imwillx@users.noreply.github.com>
2018-09-15 21:31:44 -0700

    Remove nullable

--- src/main/java/com/hazebyte/crate/api/util/MoreObjects.java
+++ src/main/java/com/hazebyte/crate/api/util/MoreObjects.java
@@ -1,9 +1,7 @@
 package com.hazebyte.crate.api.util;
 
-import com.sun.istack.internal.Nullable;
-
 public class MoreObjects {
-    public static <T> T firstNonNull(@Nullable T first, @Nullable T second) {
+    public static <T> T firstNonNull(T first, T second) {
         return first != null ? first : second;
     }
 }
