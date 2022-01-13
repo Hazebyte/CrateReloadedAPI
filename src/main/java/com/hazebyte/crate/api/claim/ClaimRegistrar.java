b49ecbd9cf7f0b5f1dd9ffdcf119328183a86d4c
imWillX <imWillX@users.noreply.github.com>
2022-01-12 22:42:32 -0800

    feat: add claim api feat: add ClaimGiveEvent. close #56

---
+++ src/main/java/com/hazebyte/crate/api/claim/ClaimRegistrar.java
@@ -0,0 +1,32 @@
+package com.hazebyte.crate.api.claim;
+
+import com.hazebyte.crate.api.crate.reward.Reward;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * ClaimRegistrar is an API that manages claims.
+ */
+public interface ClaimRegistrar {
+
+    CompletableFuture<Claim> addClaim(OfflinePlayer player, Reward... rewards) throws IOException;
+
+    CompletableFuture<Void> removeClaim(Claim claim) throws IOException;
+
+    CompletableFuture<Optional<Claim>> getClaim(OfflinePlayer player, UUID uuid);
+
+    CompletableFuture<Collection<Claim>> getClaim(OfflinePlayer player, long timestamp);
+
+    CompletableFuture<Collection<Claim>> getClaims(OfflinePlayer player);
+
+    void openInventory(Player player);
+
+    void spoofInventory(OfflinePlayer player, Player viewer);
+
+}
