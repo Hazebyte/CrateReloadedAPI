64abaa303178bf65ae4078ffd353ea5cd96995ec
imWillX <imWillX@users.noreply.github.com>
2022-03-28 01:00:54 -0700

    chore: update class naming, add CrateRemoveEvent, update effect access to something more logical

---
+++ src/main/java/com/hazebyte/crate/api/event/CrateSetEvent.java
@@ -0,0 +1,52 @@
+package com.hazebyte.crate.api.event;
+
+import com.hazebyte.crate.api.crate.Crate;
+import org.bukkit.Location;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * This represents when a crate has been set to a location.
+ * This may be triggered via the API or through a user command.
+ */
+public class CrateSetEvent extends Event implements Cancellable {
+
+    private Crate crate;
+    private Location location;
+    private boolean cancelled;
+
+    private static HandlerList handlerList = new HandlerList();
+
+    public CrateSetEvent(Crate crate, Location location) {
+        this.crate = crate;
+        this.location = location;
+    }
+
+    public Crate getCrate() {
+        return crate;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+}
