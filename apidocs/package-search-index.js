aa256137e22d96510dcc40c1f81bfd1577554d00
imWillX <imWillX@users.noreply.github.com>
2022-07-04 07:12:37 +0000

    Deploying to gh-pages from @ Hazebyte/CrateReloadedAPI@cc99f1b34f10d73002edbed113c69dcea6697347 🚀

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.hazebyte.crate.api"},{"l":"com.hazebyte.crate.api.crate"},{"l":"com.hazebyte.crate.api.crate.reward"},{"l":"com.hazebyte.crate.api.effect"},{"l":"com.hazebyte.crate.api.event"},{"l":"com.hazebyte.crate.api.exception"},{"l":"com.hazebyte.crate.api.util"}]
