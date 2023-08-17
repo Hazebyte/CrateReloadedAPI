39641ea2c1a2833254b237bce9129e3ec34d60df
imWillX <imWillX@users.noreply.github.com>
2023-08-17 00:11:28 -0700

    chore: update message to notify user of reload command side effects

--- src/main/java/com/hazebyte/crate/api/util/Messages.java
+++ src/main/java/com/hazebyte/crate/api/util/Messages.java
@@ -5,16 +5,16 @@ package com.hazebyte.crate.api.util;
  */
 public interface Messages {
 
-    String UNAVAILABLE_FEATURE          =   "&fThe following feature is not available: %s";
-    String UNAVAILABLE_FEATURE_INFO     =   UNAVAILABLE_FEATURE + " [see %s]";
-    String MESSAGE_NOT_FOUND            =   "The following message key was not found: ";
-    String RELOAD                       =   "The plugin has been reloaded.";
-    String WARNING                      =   "&eSomething happened: &f";
-    String WARNING_S                    =   WARNING + " %s";
-    String WARNING_S_INFO               =   WARNING + " %s [see %s]";
-    String ERROR                        =   "&4An error has occurred: &f";
-    String ERROR_LINE                   =   ERROR + "Parsing \"%s\"";
-    String ERROR_LINE_INFO              =   ERROR + "Parsing \"%s\" [see %s]";
-    String FORMAT                       =   "Please check the format: ";
-    String FORMAT_ITEM                  =   FORMAT + "ITEM \"%s\" [see %s]";
+  String UNAVAILABLE_FEATURE = "&fThe following feature is not available: %s";
+  String UNAVAILABLE_FEATURE_INFO = UNAVAILABLE_FEATURE + " [see %s]";
+  String MESSAGE_NOT_FOUND = "The following message key was not found: ";
+  String RELOAD = "The plugin has been reloaded. Please note that this command may cause unintended behavior. If you encounter any issues, please restart the server.";
+  String WARNING = "&eSomething happened: &f";
+  String WARNING_S = WARNING + " %s";
+  String WARNING_S_INFO = WARNING + " %s [see %s]";
+  String ERROR = "&4An error has occurred: &f";
+  String ERROR_LINE = ERROR + "Parsing \"%s\"";
+  String ERROR_LINE_INFO = ERROR + "Parsing \"%s\" [see %s]";
+  String FORMAT = "Please check the format: ";
+  String FORMAT_ITEM = FORMAT + "ITEM \"%s\" [see %s]";
 }
